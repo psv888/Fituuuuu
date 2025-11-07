0ea4027a66bdb34a797c717f88b86151533d32e7
pasupuleti satya vardhan <131777180+psv888@users.noreply.github.com>
2025-11-07 18:07:40 +0530

    Update REDIRECT_URI for OAuth configuration

--- public/config.js
+++ public/config.js
@@ -10,7 +10,7 @@ export const OAUTH_CONFIG = {
 	AUTHORIZATION_ENDPOINT: 'https://accounts.google.com/o/oauth2/v2/auth',
 	TOKEN_ENDPOINT: 'https://oauth2.googleapis.com/token', // not used on client when backend exchange is enabled
 	CLIENT_ID: '88270120327-n3keglhq3jgntq77t7curhas0b8m9n6k.apps.googleusercontent.com',
-	REDIRECT_URI: 'http://localhost:3001/',
+	REDIRECT_URI: 'http://www.yrel.site/',
 	SCOPE: 'openid profile email https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.body.read',
 };
 
